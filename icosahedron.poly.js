40569706109c6efc6338190729c588bd3f59eed3
Moofkin <cclauren15@gmail.com>
2015-05-30 00:18:32 -0700

    Eleventh Commit - Refraction Continued
    Played Around a little more with the refraction and made things "look
    better." TODO: environment mapping is upside-down. Added two more
    models. Deleted the Chevy model (or the beginnings of it).

---
+++ icosahedron.poly.js
@@ -0,0 +1,22 @@
+var ICOSAHEDRON_POLY = [
+	["icosahedron",2,3,7],
+	["icosahedron",2,8,3],
+	["icosahedron",4,5,6],
+	["icosahedron",5,4,9],
+	["icosahedron",7,6,12],
+	["icosahedron",6,7,11],
+	["icosahedron",10,11,3],
+	["icosahedron",11,10,4],
+	["icosahedron",8,9,10],
+	["icosahedron",9,8,1],
+	["icosahedron",12,1,2],
+	["icosahedron",1,12,5],
+	["icosahedron",7,3,11],
+	["icosahedron",2,7,12],
+	["icosahedron",4,6,11],
+	["icosahedron",6,5,12],
+	["icosahedron",3,8,10],
+	["icosahedron",8,2,1],
+	["icosahedron",4,10,9],
+	["icosahedron",5,9,1]
+];
